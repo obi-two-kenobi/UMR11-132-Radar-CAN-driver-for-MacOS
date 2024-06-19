4626aa2d696b354900b9ac6708f2b4e20b6a871f
Ali <ali.akalhalabi001@stud.fh-dortmund.de>
2024-06-19 14:48:07 +0200

    Initial Commit

---
+++ UMR11-132/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  UMR11-132
+//
+//  Created by Ali Alhalabi on 19.06.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
